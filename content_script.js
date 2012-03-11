f2765b758ff2395e33a3db768945249fb7e56bbd
Brian Williams <b.willums@gmail.com>
2012-03-11 19:06:01 -0400

    Add some error handling and made options page a wee bit nicer

--- content_script.js
+++ content_script.js
@@ -1,18 +1,30 @@
+// Get replace words from background.html and run main function on return.
 chrome.extension.sendRequest({method: "getLocalStorage", key: "replace_words"}, goutItOut);
 
 function goutItOut(response)
 {
-	dict = response.data.split(',');
-	console.log(dict);
+	if(response.data != "")
+	{
+		//Split on newline or comma
+		dict = response.data.split(/\r\n|\r|\n|,/g);
 	
-	jQuery("*").each(function () {
-	if (jQuery(this).children().length == 0) {
-		$currElem = jQuery(this);
-		for(i=0;i<dict.length;i++)
-		{
-			reg = new RegExp($.trim(dict[i]), "i");
-			$currElem.text($currElem.text().replace(reg, 'Gout'));
-		}
-	 }
-	});
+		jQuery("*").each(function () {
+		if (jQuery(this).children().length == 0) {
+			$currElem = jQuery(this);
+			for(i=0;i<dict.length;i++)
+			{
+				temp = $.trim(dict[i]);
+				if(temp != "")
+				{
+					reg = new RegExp(temp, "i");
+					$currElem.text($currElem.text().replace(reg, "Gout"));
+				}
+			}
+		 }
+		});
+	}
+	else
+	{
+		alert("Slow down Cowboy, Gouterizer has no words to replace! Please visit the Gouterizer options page so we can get to Gouterizing!");
+	}
 }
